5ea4c820512ac4dbef962322d98fd00b5d03990a
phamngocsonad <91709914+phamngocsonad@users.noreply.github.com>
2023-02-04 15:29:18 +0700

    3:29

---
+++ b1.java
@@ -0,0 +1,7 @@
+// bài 1: k-subsequence bounded weight 
+// dãy số nguyên a1,...an. Một dãy con k- phân tử 
+// của dãy đã cho là một dãy gồm k phần tử liên tiếp
+// a1,.. ak+1. Trọng  số của dãy con k- phần tử là tổng
+// của các phần tử trong dãy con này. Cho các số nguyên dương 
+// k, A và B. Tính Q là số lượng dãy con k phần tử sao cho trọng số 
+// của mỗi dãy con đề lơn hơn hoặc bằng A và nhỏ hơn hoặc bằng B
