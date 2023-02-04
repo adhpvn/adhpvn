453238d86f75213b705687ab083142ec2bf4625d
phamngocsonad <91709914+phamngocsonad@users.noreply.github.com>
2023-02-04 16:56:20 +0700

    4:56

--- 20205124_bai2.cpp
+++ 20205124_bai2.cpp
@@ -1,21 +1,39 @@
-#include <iostream>
-#include <queue>
+ #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+vector<int> nums;
+
+void insert(int x) {
+    nums.push_back(x);
+}
+
+int deleteMax() {
+    int maxNum = *max_element(nums.begin(), nums.end());
+    nums.erase(remove(nums.begin(), nums.end(), maxNum), nums.end());
+    return maxNum;
+}
+
 int main() {
-int n;
-cin >> n;
-priority_queue<int> q;
-string operation;
-while (cin >> operation && operation != "*") {
-if (operation == "insert") {
-int x;
-cin >> x;
-q.push(x);
-} else if (operation == "delete-max") {
-cout << q.top() << endl;
-q.pop();
-}
-}
-return 0;
+    int n;
+    cin >> n;
+    int x;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        insert(x);
+    }
+
+    string command;
+    while (cin >> command && command != "*") {
+        if (command == "delete-max") {
+            cout << deleteMax() << endl;
+        } else if (command == "insert") {
+            int x;
+            cin >> x;
+            insert(x);
+        }
+    }
+
+    return 0;
 }
