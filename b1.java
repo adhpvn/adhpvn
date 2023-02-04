53b6a57686d73c07b3b2e857dd2dbad4980a2e40
phamngocsonad <91709914+phamngocsonad@users.noreply.github.com>
2023-02-04 15:36:26 +0700

    3:36

--- b1.java
+++ b1.java
@@ -5,3 +5,8 @@
 // của các phần tử trong dãy con này. Cho các số nguyên dương 
 // k, A và B. Tính Q là số lượng dãy con k phần tử sao cho trọng số 
 // của mỗi dãy con đề lơn hơn hoặc bằng A và nhỏ hơn hoặc bằng B
+
+// input
+// dòng 1: chứa 3 số nguyên dương n, k, A, B ( 1=<n=< 10^5, 1=<k-<n/2, 1=<A,b=<10^6)
+// dòng 2L cgwas a1,.. an (1=<n=<100)
+// output: ghi ra giá trị Q
--- b2.java
+++ b2.java
@@ -1,2 +1,4 @@
 // Perform insertion and Delete-Max on a collention
-Cho một tập gồm các số nguyên a1, a2,..an
+Cho một tập gồm các số nguyên a1, a2,..an. Thực hiện trên tập số nguyên đã cho một dãy thao tác gồm 2 loại sau:
+- insert x: thêm một phần tử x vào tập
+- delete-max: xóa khỏi tập và trả về giá trị của phần tử lớn nhất trong tập
