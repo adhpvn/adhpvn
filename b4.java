362c55d147b87318a52d24108a87dbccbe19102a
phamngocsonad <91709914+phamngocsonad@users.noreply.github.com>
2023-02-04 15:52:42 +0700

    3:52

---
+++ b4.java
@@ -0,0 +1,17 @@
+// Max subsequence No 3 adjacent elements selected
+Cho một dãy gồm các số nguyên dương a1, a2, .. an.
+Tìm tập con chủa dãy đã cho sao cho tổng các phần tử trong tập con là lớn nhất
+và trong tập con không chứa 3 phần tử liên tiếp nào của dãy đã cho.
+
+// input
+dòng 1: chứa 1 số nguyên dương n ( 1=< n =< 100000)
+dòng 2: chứa a1,a2,..an (1=<ai=<1000)
+
+// output 
+ghi ra tổng của các phần tử trong tập con tìm được
+
+stdin                   stdout
+7                       31
+4 10 6 6 6 2 5
+
+// giải thích: tập con tìm đc là 4,10,6,6,5
