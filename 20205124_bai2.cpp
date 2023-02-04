3e7b6ea1e20a1d8e6e55e27ad00ebee74799ae44
phamngocsonad <91709914+phamngocsonad@users.noreply.github.com>
2023-02-04 17:13:32 +0700

    5:13

--- 20205124_bai2.cpp
+++ 20205124_bai2.cpp
@@ -3,19 +3,24 @@
 using namespace std;
 int x;
 
-int main() {
-int n;
-cin >> n;
-priority_queue<int> q;
-string operation;
-while (cin >> operation && operation != "*") {
-if (operation == "insert") {
-cin >> x;
-q.push(x);
-} else if (operation == "delete-max") {
-q.pop();
-cout << q.top() << endl;
-}
-}
-return 0;
+int main()
+{
+    int n;
+    cin >> n;
+    priority_queue<int> q;
+    string operation;
+    while (cin >> operation && operation != "*")
+    {
+        if (operation == "insert")
+        {
+            cin >> x;
+            q.push(x);
+        }
+        else if (operation == "delete-max")
+        {
+            q.pop();
+            cout << q.top() << endl;
+        }
+    }
+    return 0;
 }
