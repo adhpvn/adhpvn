b39f250381d43c97811e6eae1c488fafb88128bf
phamngocsonad <91709914+phamngocsonad@users.noreply.github.com>
2023-02-04 15:39:00 +0700

    3:38

--- b2.java
+++ b2.java
@@ -2,3 +2,10 @@
 Cho một tập gồm các số nguyên a1, a2,..an. Thực hiện trên tập số nguyên đã cho một dãy thao tác gồm 2 loại sau:
 - insert x: thêm một phần tử x vào tập
 - delete-max: xóa khỏi tập và trả về giá trị của phần tử lớn nhất trong tập
+
+// input:
+- dòng 1: 1 số nguyên n(1=<n=<100000)
+- dòng 2L a1,a2,..an
+- các dòng tiếp theo, mỗi dòng chứa 1 thao tác với định dạng như đã đề cập ở trên, đầu vào input kết thúc bởi 1 dòng chứa duy nhất ký tự *
+// output:
+đưa ra giá trị trả về của mỗi thao tác delete-max có trong đầu vào input
