fa7643d663449df654bb4f2a1e3123c136c0112a
phamngocsonad <91709914+phamngocsonad@users.noreply.github.com>
2023-03-11 15:40:12 +0700

    a

--- 20205124_P01.cpp
+++ 20205124_P01.cpp
@@ -4,14 +4,21 @@ int code;
 
 
 int NUMBER_PEOPLE(){
-
+  for 
 };
 int NUMBER_PEOPLE_BORN_AT(){
   for 
 };
+int MOST_ALIVE_ANCESTOR(){
+
+};
+int NUMBER_PEOPLE_BORN_BETWEEN
 
 
 int main() {
+
+  cin << code << date_of_birth << father_code << mother_code << is_alive << region << endln ;
+
 
 
 
